99eed0cf4dbc69fe6a96799bc0c5f8cda2eaad89
Emmanuel Tierra <emmantierra.dev@gmail.com>
2020-11-27 18:37:17 +0800

    feat: use Api for handling requests

--- todo-app/src/Server.hs
+++ todo-app/src/Server.hs
@@ -4,22 +4,16 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-import Routes (Routes)
+import Api
 import Todo 
 
 startApp :: IO ()
 startApp = run 8080 app
 
 app :: Application
-app = serve api server
+app = serve api handlers
 
 api :: Proxy Routes
 api = Proxy
 
-server :: Server Routes
-server = return todos
 
-todos :: [Todo]
-todos = [ Todo 1 "Walk the dog" False
-        , Todo 2 "Clean the kitchen" False
-        ]
